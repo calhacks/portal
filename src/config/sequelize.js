e779d18c6397e337ea0a93209e7c7aade560a8cf
Aniruddh Iyengar <aniiyengar@berkeley.edu>
2018-07-24 21:55:53 -0700

    db host

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,7 +4,7 @@ export default {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWD,
         database: process.env.DB_NAME,
-        host: '127.0.0.1',
+        host: process.env.DB_HOST,
         dialect: 'mysql'
     }
 };
