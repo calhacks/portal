0337faf308b581e2102a39e6788582d1f01ea6ee
Eric Kong <eric.kong98@gmail.com>
2018-09-23 17:44:43 -0700

    Next/Prev Button

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -71,13 +71,12 @@ export default {
 
     // Get a User's App
     getApp: (req, res, next) => {
-      console.log("-----------------------------------");
-      console.log(req['body']['id']);
+      // console.log("-----------------------------------");
+      // console.log(req['body']['id']);
       var app_str = "SELECT A.*, U.firstname, U.lastname, U.email FROM Applications as A INNER JOIN Users as U ON A.userId = U.id WHERE U.id =" + req['body']['id'];
       const queries = {
           app: app_str,
       };
-      console.log(queries['app']);
       // executes all queries and puts them in a promise
       Promise.all(Object.keys(queries).map(query => new Promise(resolve => {
           sequelize.query(queries[query]).spread((results, meta) => {
