bb3717f25af7cbb25638a04a1a470d1a4af9d5ea
Aniruddh Iyengar <aniiyengar@berkeley.edu>
2018-09-26 22:39:45 -0700

    hotfix: id -> appId

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -241,6 +241,7 @@ export default {
             'select ' +
 
             'u.id id,' +
+            'a.id appId' +
             'u.firstname firstname, ' +
             'u.lastname lastname, ' +
             'u.email email, ' +
