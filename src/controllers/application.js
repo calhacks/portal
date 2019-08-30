5a8ba811911ac829dfb817976e72e7e955371933
Adnaan Sachidanandan <adnaan15@gmail.com>
2019-08-29 20:45:53 -0700

    Added file storage with digital ocean spaces, database storage with digital ocean databases, and ui look updates

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -9,28 +9,112 @@ export default {
     submitApp: (req, res, next) => {
         // TODO: Add field validation
 
-        const saveFile = () => {
-            const resume = req.files.resume;
-            const thumbnail = req.files.thumbnail;
+        const types = {
+            'pdf': 'application/pdf',
+            'rtf': 'application/rtf',
+            'jpg': 'image/jpeg',
+            'png': 'image/png',
+            'txt': 'text/plain',
+            'rtf': 'application/rtf',
+            'doc': 'application/msword',
+            'dot': 'application/msword',
+            'docx': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        }
 
-            const result = [];
+        const result = [];
 
+        const resume = req.files.resume;
+        const thumbnail = req.files.thumbnail;
+
+        const aws = require('aws-sdk');
+        aws.config.update({
+            accessKeyId: process.env.DO_ACCESS_KEY,
+            secretAccessKey: process.env.DO_SECRET_KEY,
+        })
+        const endpoint = new aws.Endpoint(process.env.DO_MAIN_LOC);
+
+        const s3 = new aws.S3({
+            endpoint: endpoint,
+            accessKeyId: process.env.DO_ACCESS_KEY,
+            secretAccessKey: process.env.DO_SECRET_KEY,
+        });
+        
+        const options = {
+            partSize: 10 * 1024 * 1024, // 10 MB
+            queueSize: 10
+        };
+        
+        let resumeLink;
+        let thumbnailLink;
+
+        const uploadResume = (user) => {
             if (resume.size !== 0) {
                 console.log('detected resume');
                 const lst = resume.name.split('.');
+
+                let extension;
+
                 let newFilename;
                 if (lst.length > 1) {
-                    newFilename = `resume-${req.user.id}.${lst[lst.length - 1]}`;
+                    extension = lst[lst.length - 1];
+                    newFilename = `resume-${req.user.id}.${extension}`;
                 } else {
                     newFilename = `resume-${req.user.id}`;
                 }
 
                 const oldPath = resume.path;
-                const newPath = path.resolve(homedir, `resumes/${newFilename}`);
 
-                result.push(fs.move(oldPath, newPath, { overwrite: true }));
+                let contentType = 'application/octet-stream';
+                if (extension in types) {
+                    contentType = types[extension];
+                }
+
+                console.log(user.Application);
+                
+                if (user.Application !== null && user.Application.resume !== null) {
+                    const split = user.Application.resume.split('.');
+                    if (split.length > 1) {
+                        const oldExtension = split[split.length - 1];
+                        if (oldExtension !== extension) {
+                            const params = {
+                                Bucket: process.env.DO_BUCKET,
+                                Key: `resumes/resume-${req.user.id}.${oldExtension}`
+                            };
+                            console.log(params);
+                            s3.deleteObject(params, function(err, data) {
+                                if (err) console.log(err, err.stack);  // error
+                                else     console.log();                 // deleted
+                            });
+                        }
+                    }
+                }
+
+                const params = {
+                    Bucket: process.env.DO_BUCKET,
+                    Key: 'resumes/' + newFilename,
+                    Body: fs.createReadStream(oldPath),
+                    ACL: 'public-read',
+                    ContentType: contentType
+                };
+                console.log("created params");
+                console.log(params);
+                s3.upload(params, options, function (err, data) {
+                    if (!err) {
+                        console.log("upload complete");
+                        console.log(data); // successful response
+                        console.log(data.Location);
+                        resumeLink = data.Location;
+                        uploadThumbnail(user);
+                    } else {
+                        console.log(err); // an error occurred
+                    }
+                });
+            } else {
+                uploadThumbnail(user);
             }
+        }
 
+        const uploadThumbnail = (user) => {
             if (thumbnail.size !== 0) {
                 console.log('detected thumb');
                 const lst = thumbnail.name.split('.');
@@ -42,61 +126,83 @@ export default {
                 }
 
                 const oldPath = thumbnail.path;
-                const newPath = path.resolve(homedir, `pics/${newFilename}`);
 
-                result.push(fs.move(oldPath, newPath, { overwrite: true }));
+                if (user.Application !== null && user.Application.thumbnail !== null) {
+                    const split = user.Application.thumbnail.split('.');
+                    if (split.length > 1) {
+                        const oldExtension = split[split.length - 1];
+                        if (oldExtension !== extension) {
+                            const params = {
+                                Bucket: process.env.DO_BUCKET,
+                                Key: `thumbnails/thumbnail-${req.user.id}.${oldExtension}`
+                            };
+                            s3.deleteObject(params, function(err, data) {
+                                if (err) console.log(err, err.stack);  // error
+                                else     console.log();                 // deleted
+                            });
+                        }
+                    }
+                }
+
+                console.log('reached2');
+                const params = {
+                    Bucket: process.env.DO_BUCKET,
+                    Key: 'thumbnails/' + newFilename,
+                    Body: fs.createReadStream(oldPath),
+                    ACL: 'public-read'
+                };
+                console.log("created params2");
+                console.log(params);
+                s3.upload(params, options, function (err, data) {
+                    if (!err) {
+                        console.log(data); // successful response
+                        console.log(data.Location);
+                        thumbnailLink = data.Location;
+                        console.log("upload complete2");
+                        completeUpload(user);
+                    } else {
+                        console.log(err);
+                    }
+                });
+            } else {
+                completeUpload(user);
             }
-
-            return Promise.all(result);
         }
 
-        User.findOne({
-            where: { id: req.user.id },
-            include: { model: Application }
-        }).then(user => {
+        const startApplicationUpload = () => {
+            User.findOne({
+                where: { id: req.user.id },
+                include: { model: Application }
+            }).then(user => {
+                uploadResume(user);
+            }).catch(err => {
+                res.send(err);
+            });
+        }
+
+        const completeUpload = (user) => {
             if (user.Application === null) {
                 var data = req.body;
-                data['resume'] = req.files.resume.name;
+                console.log("resume");
+                console.log(result.length);
+                data['resume'] = resume.name;
+                data['thumbnail'] = thumbnail.name;
                 Application.create({
                     ...data,
                     UserId: req.user.id
-                }).then(newApp => {
-                    // App has been created.
-                    saveFile().then(() => {
-                        console.log('File moved successfully!');
-                        res.redirect('/dashboard');
-                    }).catch(err => {
-                        console.log(err);
-                        res.redirect('/dashboard');
-                    });
-                }).catch(err => {
-                    res.send(err);
                 });
             } else {
                 console.log(req.files.resume.name);
                 var data = req.body;
-                data['resume'] = req.files.resume.name;
-                if (req.files.thumbnail.size !== 0) {
-                    data['thumbnail'] = req.files.thumbnail.name;
-                }
-                
-                user.Application.updateAttributes(data).then(newApp => {
-                    // App has been saved.
-                    console.log(req.body)
-                    saveFile().then(() => {
-                        console.log('File moved successfully!');
-                        res.redirect('/dashboard');
-                    }).catch(err => {
-                        console.log(err);
-                        res.redirect('/dashboard');
-                    });
-                }).catch(err => {
-                    res.send(err);
-                });
+                console.log("resume");
+                console.log(result[0]);
+                data['resume'] = resume.name;
+                data['thumbnail'] = thumbnail.name;
+                user.Application.updateAttributes(data);
             }
-        }).catch(err => {
-            res.send(err);
-        });
+            res.redirect('/dashboard');
+        }
+        startApplicationUpload();
     },
 
     appPage: (req, res) => {
@@ -106,7 +212,7 @@ export default {
                 { model: Application }
             ]
         }).then(user => {
-            res.render('application', { user: user.toJSON() })
+            res.render('application', { user: user.toJSON() , school: user.Application.school })
         });
     }
 }
