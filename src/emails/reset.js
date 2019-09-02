7b3c6c4e487a68685478b343935dff1ece9587b6
Adnaan Sachidanandan <adnaan15@gmail.com>
2019-09-02 16:51:31 -0700

    modified emails to have portal instead of 2018

--- src/emails/reset.js
+++ src/emails/reset.js
@@ -5,8 +5,8 @@ Dear ${name},
 <br/>
 <br/>
 You can reset your password by clicking on the link below:
-<a href="https://2018.calhacks.io/reset?code=${code}">
-    https://2018.calhacks.io/reset?code=${code}
+<a href="https://portal.calhacks.io/reset?code=${code}">
+    https://portal.calhacks.io/reset?code=${code}
 </a><br/>
 If you didn't request a new password, please ignore this email!
 </p>
--- src/emails/verify.js
+++ src/emails/verify.js
@@ -6,8 +6,8 @@ Dear ${name},
 <br/>
 <strong>Thanks so much for making an account with us!</strong><br/>
 To make sure it's you, please verify your email at this link:
-<a href="https://2018.calhacks.io/verify?code=${code}">
-    https://2018.calhacks.io/verify?code=${code}
+<a href="https://portal.calhacks.io/verify?code=${code}">
+    https://portal.calhacks.io/verify?code=${code}
 </a><br/>
 If you didn't make an account on the Cal Hacks portal, please ignore this email!
 </p>
