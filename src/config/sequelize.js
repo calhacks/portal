4f804d9628436d3f2635da6debb83d2616cb8f72
Terrance Li <terranceli.terbear@gmail.com>
2019-09-22 18:09:03 -0700

    Uncommented Sequelize SSL options for dev env

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,23 +1,22 @@
-
 export default {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        // ssl: true,
-        // dialectOptions: { ssl: {require:true }}
-    },
-    production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        ssl: true,
-        dialectOptions: { ssl: {require:true }}
-    }
-};
+	development: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'postgres',
+		ssl: true,
+		dialectOptions: { ssl: { require: true } }
+	},
+	production: {
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWD,
+		database: process.env.DB_NAME,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		dialect: 'postgres',
+		ssl: true,
+		dialectOptions: { ssl: { require: true } }
+	}
+}
