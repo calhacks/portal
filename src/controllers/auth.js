22eff94405c8a5625abaff2cb4eda844118de4f7
Terrance Li <tyli@berkeley.edu>
2019-09-23 16:28:06 -0700

    Session persist (#56)
    * Add supersenior option
    
    * Session persisting
    
    * Revert "Add supersenior option"
    
    This reverts commit 3ab92369ca0387c686fd5638ce0b96bb6537a369.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,173 +1,186 @@
-
-import passport from 'passport';
-import sendgrid from '@sendgrid/mail';
-import { User } from '../models/index';
-import reset from '../emails/reset';
-import bcrypt from 'bcrypt-nodejs';
-var Sequelize = require('sequelize');
+import passport from 'passport'
+import sendgrid from '@sendgrid/mail'
+import { User } from '../models/index'
+import reset from '../emails/reset'
+import bcrypt from 'bcrypt-nodejs'
+var Sequelize = require('sequelize')
 
 const genUUID = () => {
-    const chars = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890';
-    let result = '';
-    for (let i = 0; i < 10; i++) {
-        result = result + chars[Math.floor(Math.random() * 26)];
-    }
-    return result;
+	const chars = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890'
+	let result = ''
+	for (let i = 0; i < 10; i++) {
+		result = result + chars[Math.floor(Math.random() * 26)]
+	}
+	return result
 }
 const generateHash = pass => {
-    return bcrypt.hashSync(pass, bcrypt.genSaltSync(8), null);
-};
+	return bcrypt.hashSync(pass, bcrypt.genSaltSync(8), null)
+}
 
 export default {
-    signIn: (req, res, next) => {
-        return res.render('login');
-    },
+	signIn: (req, res, next) => {
+		// check if valid session exists
+		if (req.user) {
+			// redirect to /dashboard
+			return res.redirect('/dashboard')
+		} else {
+			return res.render('login')
+		}
+	},
 
-    signUp: (req, res, next) => {
-        return res.render('signup');
-    },
+	signUp: (req, res, next) => {
+		return res.render('signup')
+	},
 
-    logout: (req, res, next) => {
-        return req.session.destroy(err => {
-            res.redirect('/login');
-        });
-    },
+	logout: (req, res, next) => {
+		return req.session.destroy(err => {
+			res.redirect('/login')
+		})
+	},
 
-    submitLogin: (req, res, next) => {
-        passport.authenticate('local-signin', (err, user, info) => {
-            if (err !== null) {
-                // Something went wrong.
-                req.flash('error', 'There was an unexpected error signing in.');
-                return res.redirect('/login');
-            } else if (!user) {
-                req.flash('error', 'Invalid email/password combination.');
-                return res.redirect('/login');
-            } else {
-                req.logIn(user, err => {
-                    if (err) {
-                        req.flash('error', 'There was an unexpected error signing in.');
-                        return res.redirect('/login');
-                    } else {
-                        return res.redirect('/dashboard');
-                    }
-                });
-            }
-        })(req, res, next);
-    },
+	submitLogin: (req, res, next) => {
+		passport.authenticate('local-signin', (err, user, info) => {
+			if (err !== null) {
+				// Something went wrong.
+				req.flash('error', 'There was an unexpected error signing in.')
+				return res.redirect('/login')
+			} else if (!user) {
+				req.flash('error', 'Invalid email/password combination.')
+				return res.redirect('/login')
+			} else {
+				req.logIn(user, err => {
+					if (err) {
+						req.flash('error', 'There was an unexpected error signing in.')
+						return res.redirect('/login')
+					} else {
+						return res.redirect('/dashboard')
+					}
+				})
+			}
+		})(req, res, next)
+	},
 
-    submitSignup: (req, res, next) => {
-        passport.authenticate('local-signup', (err, user, info) => {
-            if (err !== null) {
-                // Something went wrong.
-                req.flash('error', JSON.stringify(err));
-                return res.redirect('/signup');
-            } else if (!user) {
-                req.flash('error', info.message);
-                return res.redirect('/signup');
-            } else {
-                req.logIn(user, err => {
-                    if (err) {
-                        req.flash('error', 'There was an unexpected error creating an account.');
-                        return res.redirect('/signup');
-                    } else {
-                        return res.redirect('/dashboard');
-                    }
-                });
-            }
-        })(req, res, next);
-    },
+	submitSignup: (req, res, next) => {
+		passport.authenticate('local-signup', (err, user, info) => {
+			if (err !== null) {
+				// Something went wrong.
+				req.flash('error', JSON.stringify(err))
+				return res.redirect('/signup')
+			} else if (!user) {
+				req.flash('error', info.message)
+				return res.redirect('/signup')
+			} else {
+				req.logIn(user, err => {
+					if (err) {
+						req.flash(
+							'error',
+							'There was an unexpected error creating an account.'
+						)
+						return res.redirect('/signup')
+					} else {
+						return res.redirect('/dashboard')
+					}
+				})
+			}
+		})(req, res, next)
+	},
 
-    validate: (req, res, next) => {
-        User.update({
-            emailValidated: true
-        }, {
-            where: { emailCode: req.query.code }
-        }).then(result => {
-            console.log(result);
-            if (result[0] == 0) {
-                res.render('verify', { success: false });
-            } else {
-                res.render('verify', { success: true });
-            }
-        });
-    },
+	validate: (req, res, next) => {
+		User.update(
+			{
+				emailValidated: true
+			},
+			{
+				where: { emailCode: req.query.code }
+			}
+		).then(result => {
+			console.log(result)
+			if (result[0] == 0) {
+				res.render('verify', { success: false })
+			} else {
+				res.render('verify', { success: true })
+			}
+		})
+	},
 
-    informVerify: (req, res, next) => {
-        return res.render('informVerify', { user: req.user });
-    },
+	informVerify: (req, res, next) => {
+		return res.render('informVerify', { user: req.user })
+	},
 
-    informReset: (req, res, next) => {
-        return res.render('informReset', { user: req.user });
-    },
+	informReset: (req, res, next) => {
+		return res.render('informReset', { user: req.user })
+	},
 
-    resetPassword: (req, res, next) => {
-        return res.render('resetPassword')
-    },
+	resetPassword: (req, res, next) => {
+		return res.render('resetPassword')
+	},
 
-    submitReset: (req, res, next) => {
-      User.findOne({
-        where: {
-          email: req.body.email
-        }
-      }).then(user => {
-        if (!(user)) {
-            req.flash('error', 'There is no user associated with that email.');
-            return res.redirect('/reset_password');
-        }
-        else {
-            const resetCode = genUUID();
-            const email = req.body.email;
+	submitReset: (req, res, next) => {
+		User.findOne({
+			where: {
+				email: req.body.email
+			}
+		}).then(user => {
+			if (!user) {
+				req.flash('error', 'There is no user associated with that email.')
+				return res.redirect('/reset_password')
+			} else {
+				const resetCode = genUUID()
+				const email = req.body.email
 
-            sendgrid.send({
-              to: email,
-              from: 'team@calhacks.io',
-              subject: 'Reset your password for Cal Hacks',
-              html: reset(
-                  user.firstname + ' ' + user.lastname,
-                  resetCode
-              )
-            })
+				sendgrid.send({
+					to: email,
+					from: 'team@calhacks.io',
+					subject: 'Reset your password for Cal Hacks',
+					html: reset(user.firstname + ' ' + user.lastname, resetCode)
+				})
 
-            User.update({
-                resetPasswordCode: resetCode,
-                resetPasswordExpiration: new Date()
-            }, {
-                where: { email: email }
-            }).then(result => {
-                res.render('informReset', { user: user })
-            });
-        }
-      })
-    },
+				User.update(
+					{
+						resetPasswordCode: resetCode,
+						resetPasswordExpiration: new Date()
+					},
+					{
+						where: { email: email }
+					}
+				).then(result => {
+					res.render('informReset', { user: user })
+				})
+			}
+		})
+	},
 
-    changePassword: (req, res, next) => {
-      return res.render('changePassword', {code: req.query.code});
-    },
+	changePassword: (req, res, next) => {
+		return res.render('changePassword', { code: req.query.code })
+	},
 
-    submitPassword: (req, res, next) => {
-      console.log(req.query);
-        var curr_date = new Date();
-        curr_date.setHours(curr_date.getHours() - 1);
-        console.log('hello');
-        User.update({
-            password: generateHash(req.body.password)
-        }, {
-            where: {
-              resetPasswordCode: req.query.code,
-              resetPasswordExpiration: {
-                [Sequelize.Op.gt]: curr_date
-              }
-            }
-        }).then(result => {
-            if (result[0] == 0) {
-                res.render('newPassword', { success: false });
-            } else {
-                res.render('newPassword', { success: true });
-            }
-        });
-    },
+	submitPassword: (req, res, next) => {
+		console.log(req.query)
+		var curr_date = new Date()
+		curr_date.setHours(curr_date.getHours() - 1)
+		console.log('hello')
+		User.update(
+			{
+				password: generateHash(req.body.password)
+			},
+			{
+				where: {
+					resetPasswordCode: req.query.code,
+					resetPasswordExpiration: {
+						[Sequelize.Op.gt]: curr_date
+					}
+				}
+			}
+		).then(result => {
+			if (result[0] == 0) {
+				res.render('newPassword', { success: false })
+			} else {
+				res.render('newPassword', { success: true })
+			}
+		})
+	},
 
-    newPassword: (req, res, next) => {
-      return res.render('newPassword');
-    }
-};
+	newPassword: (req, res, next) => {
+		return res.render('newPassword')
+	}
+}
