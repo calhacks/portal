4d3357f7cda9066d1b3c4fefabeae4460415fafd
Terrance Li <terranceli.terbear@gmail.com>
2019-09-24 14:19:00 -0700

    Make fix for env port

---
+++ routes.js
@@ -0,0 +1,108 @@
+
+import homeController from '../controllers/home';
+import authController from '../controllers/auth';
+import dashboardController from '../controllers/dashboard';
+import applicationController from '../controllers/application';
+import teamController from '../controllers/team';
+import cubstartController from '../controllers/cubstart';
+import adminController from '../controllers/admin';
+
+export default {
+    // Home route
+    '/': {
+        get: authController.signIn
+    },
+    // Home -> CH Code of Conduct
+    '/code-of-conduct': {
+        get: homeController.codeOfConduct,
+    },
+
+    // Auth routes
+    '/login': {
+        get: authController.signIn,
+        post: authController.submitLogin
+    },
+    '/signup': {
+        get: authController.signUp,
+        post: authController.submitSignup
+    },
+    '/logout': {
+        get: authController.logout
+    },
+    '/verify': {
+        get: authController.validate
+    },
+    '/inform_verify': {
+        get: authController.informVerify
+    },
+    '/reset_password': {
+      get: authController.resetPassword,
+      post: authController.submitReset
+    },
+    '/reset': {
+      get: authController.changePassword,
+      post: authController.submitPassword
+    },
+    '/newPassword': {
+      get: authController.newPassword
+    },
+    // Dashboard
+    '/dashboard': {
+        get: dashboardController.dashboard
+    },
+
+    // Submitting an app
+    '/application': {
+        get: applicationController.appPage,
+        post: applicationController.submitApp
+    },
+
+    // cubstart application
+    '/cubstart': {
+        get: cubstartController.cubstart,
+        post: cubstartController.submitApp
+    },
+
+    // Submitting a team
+    '/team': {
+        post: teamController.submitTeam,
+    },
+
+    // Leaving a team
+    '/leaveTeam': {
+        post: teamController.leaveTeam,
+    },
+
+    // God mode
+    '/stats': {
+        get: adminController.stats,
+    },
+    '/roster': {
+        get: adminController.roster,
+    },
+    '/deify': {
+        post: adminController.deify,
+    },
+    '/scoring': {
+        get: adminController.scoring,
+    },
+    '/app': {
+        get: adminController.getApp
+    },
+    '/score': {
+        post: adminController.postScore,
+        get: adminController.getScore
+    },
+    '/findApp': {
+        get: adminController.findApp,
+    },
+    '/resume': {
+        get: adminController.loadResume,
+    },
+    '/accumulate': {
+        get: adminController.accumulate,
+    },
+    '/scoringStats': {
+        get: adminController.scoringStats,
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,111 +1,97 @@
+var path = require('path')
+var webpack = require('webpack')
+var nodeExternals = require('webpack-node-externals')
 
-var path = require('path');
-var webpack = require('webpack');
-var nodeExternals = require('webpack-node-externals');
-
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+require('dotenv').config()
 
 var baseRules = [
-    {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: /src/,
-        exclude: /node_modules|public/,
-        resolve: {
-            extensions: ['.js', '.jsx', '.sass']
-        }
-    }
-];
+	{
+		test: /\.jsx?$/,
+		loader: 'babel-loader',
+		include: /src/,
+		exclude: /node_modules|public/,
+		resolve: {
+			extensions: ['.js', '.jsx', '.sass']
+		}
+	}
+]
 
 var devServerConfig = {
-    name: 'server',
-    mode: 'development',
-    entry: [path.resolve(__dirname, 'src/app.js')],
-    resolve: {
-        modules: [
-            path.resolve('./src'),
-            'node_modules'
-        ]
-    },
-    module: {
-        rules: baseRules
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'server.js'
-    },
-    target: 'node',
-    externals: [nodeExternals()],
-    plugins: [
-        new CopyWebpackPlugin(
-            [
-                {
-                    from: 'src/client/assets',
-                    to: 'assets'
-                }
-            ]
-        ),
-    ]
+	name: 'server',
+	mode: 'development',
+	entry: [path.resolve(__dirname, 'src/app.js')],
+	resolve: {
+		modules: [path.resolve('./src'), 'node_modules']
+	},
+	module: {
+		rules: baseRules
+	},
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'server.js'
+	},
+	target: 'node',
+	externals: [nodeExternals()],
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: 'src/client/assets',
+				to: 'assets'
+			}
+		])
+	]
 }
 
 var prodServerConfig = {
-    name: 'server',
-    mode: 'production',
-    entry: [path.resolve(__dirname, 'src/app.js')],
-    resolve: {
-        modules: [
-            path.resolve('./src'),
-            'node_modules'
-        ]
-    },
-    module: {
-        rules: [
-            ...baseRules,
-            {
-                test: /\.sass$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'server.js'
-    },
-    target: 'node',
-    externals: [nodeExternals()],
-    plugins: [
-        new CopyWebpackPlugin(
-            [
-                {
-                    from: 'src/client/assets',
-                    to: 'assets'
-                },
-		{
-		    from: 'node_modules/aos/dist/aos.css',
-		    to: 'assets/css'
-		},
-		{
-		    from: 'node_modules/aos/dist/aos.js',
-		    to: 'assets/js'
-		}
-            ]
-        ),
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/[name].css'
-        })
-    ]
+	name: 'server',
+	mode: 'production',
+	entry: [path.resolve(__dirname, 'src/app.js')],
+	resolve: {
+		modules: [path.resolve('./src'), 'node_modules']
+	},
+	module: {
+		rules: [
+			...baseRules,
+			{
+				test: /\.sass$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'server.js'
+	},
+	target: 'node',
+	externals: [nodeExternals()],
+	plugins: [
+		new CopyWebpackPlugin([
+			{
+				from: 'src/client/assets',
+				to: 'assets'
+			},
+			{
+				from: 'node_modules/aos/dist/aos.css',
+				to: 'assets/css'
+			},
+			{
+				from: 'node_modules/aos/dist/aos.js',
+				to: 'assets/js'
+			}
+		]),
+		new MiniCssExtractPlugin({
+			filename: 'assets/css/[name].css'
+		})
+	]
 }
 
-var serverConfig;
+var serverConfig
 if (process.env.NODE_ENV == 'production') {
-    serverConfig = prodServerConfig;
+	serverConfig = prodServerConfig
 } else {
-    serverConfig = devServerConfig;
+	serverConfig = devServerConfig
 }
 
-module.exports = serverConfig;
+module.exports = serverConfig
