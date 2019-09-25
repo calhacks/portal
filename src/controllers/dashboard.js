43c44052a4bf41bc7a66fbe58ba0973c2a3586c3
Terrance Li <tyli@berkeley.edu>
2019-09-25 13:05:02 -0700

    App counter in God mode > Stats (#65)
    * Application counting feature
    
    * Added location differentiation and table styling

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,23 +1,36 @@
-
-import { User, Application, Team, CubStart } from '../models';
+import { User, Application, Team, CubStart } from '../models'
 
 export default {
-    dashboard: (req, res, next) => {
-        User.findOne({
-            where: { id: req.user.id },
-            include: [
-                { model: Application },
-                { model: Team, include: [User] },
-                { model: CubStart },
-            ]
-        }).then(user => {
-            if (user.role === 'hacker') {
-                res.render('dashboard', { user: user.toJSON() });
-            } else if (user.role === 'admin') {
-                res.render('adminDashboard', {
-                    user: user.toJSON()
-                });
-            }
-        });
-    }
-};
+	dashboard: (req, res, next) => {
+		User.findOne({
+			where: { id: req.user.id },
+			include: [
+				{ model: Application },
+				{ model: Team, include: [User] },
+				{ model: CubStart }
+			]
+		}).then(user => {
+			if (user.role === 'hacker') {
+				res.render('dashboard', { user: user.toJSON() })
+			} else if (user.role === 'admin') {
+				Application.findAll().then(apps => {
+					let count = apps.length
+					let berkeleyCount = apps.filter(a => a.transportation === 'berkeley')
+						.length
+					let californiaCount = apps.filter(
+						a => a.transportation === 'california'
+					).length
+					let oosCount = apps.filter(a => a.transportation === 'oos').length
+
+					res.render('adminDashboard', {
+						user: user.toJSON(),
+						count,
+						berkeleyCount,
+						californiaCount,
+						oosCount
+					})
+				})
+			}
+		})
+	}
+}
