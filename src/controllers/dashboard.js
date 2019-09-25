eb74bff14f0ead6f7dcc714d791af0db7ea96d9d
Terrance Li <terranceli.terbear@gmail.com>
2019-09-25 12:06:40 -0700

    Application counting feature

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,23 +1,30 @@
-
-import { User, Application, Team, CubStart } from '../models';
+import { User, Application, Team, CubStart } from '../models'
 
 export default {
-    dashboard: (req, res, next) => {
-        User.findOne({
-            where: { id: req.user.id },
-            include: [
-                { model: Application },
-                { model: Team, include: [User] },
-                { model: CubStart },
-            ]
-        }).then(user => {
-            if (user.role === 'hacker') {
-                res.render('dashboard', { user: user.toJSON() });
-            } else if (user.role === 'admin') {
-                res.render('adminDashboard', {
-                    user: user.toJSON()
-                });
-            }
-        });
-    }
-};
+	dashboard: (req, res, next) => {
+		User.findOne({
+			where: { id: req.user.id },
+			include: [
+				{ model: Application },
+				{ model: Team, include: [User] },
+				{ model: CubStart }
+			]
+		}).then(user => {
+			if (user.role === 'hacker') {
+				res.render('dashboard', { user: user.toJSON() })
+			} else if (user.role === 'admin') {
+				Application.count().then(count => {
+					Application.count({
+						where: { school: 'The University of California, Berkeley' }
+					}).then(berkeleyCount => {
+						res.render('adminDashboard', {
+							user: user.toJSON(),
+							count,
+							berkeleyCount
+						})
+					})
+				})
+			}
+		})
+	}
+}
--- src/models/application.js
+++ src/models/application.js
@@ -1,143 +1,146 @@
-
 export default (sequelize, DataTypes) => {
-    const Application = sequelize.define('Application', {
-        phone: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        gender: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        genderOther: {
-            type: DataTypes.TEXT,
-        },
-        school: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        year: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        bday: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true,
-            },
-        },
-        race: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true,
-            },
-        },
-        raceOther: {
-            type: DataTypes.TEXT,
-        },
-        major: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        thumbnail: {
-            type: DataTypes.TEXT
-        },
-        transportation: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        requireTransportation: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        chCodeOfConduct: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        mlhCodeOfConduct: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        mlhAffiliation: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        allergies: {
-            type: DataTypes.TEXT,
-        },
-        shirt: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        links: {
-            type: DataTypes.TEXT,
-        },
-        hackathons: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: true
-            }
-        },
-        hearAbout: {
-            type: DataTypes.ARRAY(DataTypes.TEXT),
-            validate: {
-                notEmpty: true
-            }
-        },
-        hearAboutOther: {
-            type: DataTypes.TEXT,
-            validate: {
-                notEmpty: false
-            }
-        },
-        spotlightBlurb: {
-            type: DataTypes.TEXT,
-        },
+	const Application = sequelize.define(
+		'Application',
+		{
+			phone: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			gender: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			genderOther: {
+				type: DataTypes.TEXT
+			},
+			school: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			year: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			bday: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			race: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			raceOther: {
+				type: DataTypes.TEXT
+			},
+			major: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			thumbnail: {
+				type: DataTypes.TEXT
+			},
+			transportation: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			requireTransportation: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			chCodeOfConduct: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			mlhCodeOfConduct: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			mlhAffiliation: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			allergies: {
+				type: DataTypes.TEXT
+			},
+			shirt: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			links: {
+				type: DataTypes.TEXT
+			},
+			hackathons: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: true
+				}
+			},
+			hearAbout: {
+				type: DataTypes.ARRAY(DataTypes.TEXT),
+				validate: {
+					notEmpty: true
+				}
+			},
+			hearAboutOther: {
+				type: DataTypes.TEXT,
+				validate: {
+					notEmpty: false
+				}
+			},
+			spotlightBlurb: {
+				type: DataTypes.TEXT
+			},
 
-        resume: {
-            type: DataTypes.TEXT,
-        },
+			resume: {
+				type: DataTypes.TEXT
+			},
 
-        question1: {
-            type: DataTypes.TEXT
-        },
-        question2: {
-            type: DataTypes.TEXT
-        },
-        question3: {
-            type: DataTypes.TEXT
-        },
+			question1: {
+				type: DataTypes.TEXT
+			},
+			question2: {
+				type: DataTypes.TEXT
+			},
+			question3: {
+				type: DataTypes.TEXT
+			},
 
-        beginner: {
-            type: DataTypes.TEXT
-        }
-    }, {});
+			beginner: {
+				type: DataTypes.TEXT
+			}
+		},
+		{}
+	)
 
-    Application.associate = models => {
-        models.Application.belongsTo(models.User);
-        models.Application.hasMany(models.ApplicationScore);
-    };
-    return Application;
-};
+	Application.associate = models => {
+		models.Application.belongsTo(models.User)
+		models.Application.hasMany(models.ApplicationScore)
+	}
+	return Application
+}
