965b1ddbd58004db31f10e3de2d89a28216c1523
Terrance Li <terranceli.terbear@gmail.com>
2019-09-29 00:09:49 -0700

    Add team name not empty validation

--- src/controllers/team.js
+++ src/controllers/team.js
@@ -1,55 +1,63 @@
-
-import { User, Team } from '../models';
+import { User, Team } from '../models'
 
 export default {
-    submitTeam: (req, res, next) => {
-        // Either the user wants to create a new team,
-        // or they want to join an existing one. We have
-        // to check for both
+	submitTeam: (req, res, next) => {
+		// Either the user wants to create a new team,
+		// or they want to join an existing one. We have
+		// to check for both
 
-        Team.findOne({
-            where: {
-                name: req.body.name
-            },
-            include: {
-                model: User
-            }
-        }).then(team => {
-            if (team === null) {
-                // No team found
-                Team.create({
-                    name: req.body.name
-                }).then(newTeam => {
-                    newTeam.addUser(req.user.id).then(result => {
-                        res.redirect('/dashboard#team');
-                    });
-                });
-            } else {
-                // We gotta add to the team
-                if (team.Users.length < 4) {
-                    // We can add to the team.
-                    team.addUser(req.user.id).then(result => {
-                        res.redirect('/dashboard#team');
-                    });
-                } else {
-                    // Team has too many members.
-                    req.flash('error', 'That team is already at the maximum of 4 members.');
-                    res.redirect('/dashboard#team');
-                }
-            }
-        });
-    },
+		// validate team name is not empty
+		if (!req.body.name || req.body.name.trim() === '') {
+			req.flash('error', 'Team name cannot be empty')
+			return res.redirect('/dashboard#team')
+		}
 
-    leaveTeam: (req, res, next) => {
-        // User leaves team.
+		Team.findOne({
+			where: {
+				name: req.body.name
+			},
+			include: {
+				model: User
+			}
+		}).then(team => {
+			if (team === null) {
+				// No team found
+				Team.create({
+					name: req.body.name
+				}).then(newTeam => {
+					newTeam.addUser(req.user.id).then(result => {
+						res.redirect('/dashboard#team')
+					})
+				})
+			} else {
+				// We gotta add to the team
+				if (team.Users.length < 4) {
+					// We can add to the team.
+					team.addUser(req.user.id).then(result => {
+						res.redirect('/dashboard#team')
+					})
+				} else {
+					// Team has too many members.
+					req.flash(
+						'error',
+						'That team is already at the maximum of 4 members.'
+					)
+					res.redirect('/dashboard#team')
+				}
+			}
+		})
+	},
 
-        User.findOne({
-            where: { id: req.user.id },
-            include: { model: Team }
-        }).then(user => {
-            user.setTeam(null).then(teams => {
-                res.redirect('/dashboard#team');
-            });
-        })
-    }
-};
+	leaveTeam: (req, res, next) => {
+		// User leaves team.
+
+		User.findOne({
+			where: { id: req.user.id },
+			include: { model: Team }
+		}).then(user => {
+			user.setTeam(null).then(teams => {
+				res.redirect('/dashboard#team')
+			})
+		})
+	}
+}
